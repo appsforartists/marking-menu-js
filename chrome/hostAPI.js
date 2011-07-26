3cbcfb96593c45c63369dbcb0421a87905137551
Brenton Simpson <appsforartists@users.noreply.github.com>
2011-07-26 04:20:14 +0000

    Beginning to move settings API from content script to background page.

--- chrome/hostAPI.js
+++ chrome/hostAPI.js
@@ -8,33 +8,7 @@ var hostAPI = {
 	'getURL':			function(path) {
 		return chrome.extension.getURL(path);
 	},
-	
-	'loadVariable':		function(key, callback) {
-		message = {
-			'action':	'getFromLocalStorage',
-			'key':		key,
-		}
-	
-		var setLocalVariable = function(response) {
-			window[response.key] = response.value;
-			
-			if (callback)
-				callback();
-		}
-	
-		this.sendRequest(message, setLocalVariable);
-	},
-	
-	'storeVariable':	function(key, callback) {
-		message = {
-			'action':	'storeInLocalStorage',
-			'key':		key,
-			'value':	window[key],
-		}
-
-		this.sendRequest(message, callback);
-	},
-	
+		
 	'sendRequest':		function(message, callback) {
 		callback = callback || function() {};
 		
