ec7d84fd56f3792a210440ad804fb48b10c11153
Brenton Simpson <appsforartists@users.noreply.github.com>
2011-07-31 04:22:33 +0000

    Using a unique signature to link request message to response messages.  Hopefully, this will avoid any potential collisions and make our request/response system more robust.

--- safari/hostAPI.js
+++ safari/hostAPI.js
@@ -12,30 +12,39 @@ var hostAPI = {
 	},
 	
 	'sendRequest':		function(message, callback) {
-		var action = message.action;
+		// Create a unique signature so we'll know which message the background page is responding to later.
+		var signature = [new Date().valueOf(), Math.random()].join(';');
+		
+		for (var key in message) {
+			signature += ';' + key + '=' + message[key];
+		}
+
+		message['signature'] = signature;
 		
 		_pendingResponses.push({
-			'action':	action,
-			'callback': callback
+			'signature':	signature,
+			'callback': 	callback
 		});
+		
 				
-		safari.self.tab.dispatchMessage(action, message);
+		safari.self.tab.dispatchMessage(message.action, message);
 	}
 }
 
 function onMessage(event) {
+	var action = event.name;
+	
 	/*	If this message is a response to an earlier request, call the appropriate
 	 *	callback with the arguments sent back from the background page.
 	 */
 		
-	var action = event.name;
-	
 	if (action.substr(0, MessageAPI.RESPONSE_PREFIX.length) == MessageAPI.RESPONSE_PREFIX) {
-		action = action.substr(MessageAPI.RESPONSE_PREFIX.length);
-				
+		var signature = action.substr(MessageAPI.RESPONSE_PREFIX.length);
+		
 		for (var i = 0; i < _pendingResponses.length; i++) {
-			if (_pendingResponses[i].action == action) {
-				// This presumes that the messages come back in the order they were sent; there's no other guarantee that only one message with this action will be pending.
+			if (_pendingResponses[i].signature == signature) {
+				// signature is a Safari-specific implementation detail - don't pollute the response with it.
+				delete event.message.signature;
 				
 				var currentResponse = _pendingResponses.splice(i, 1).pop();
 				
